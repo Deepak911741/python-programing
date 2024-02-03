0d4c8a2a55d3e22da9d0b81b5079a64a2d8879f1
Deepak Kumar <127077941+Deepak911741@users.noreply.github.com>
2024-02-03 19:37:14 +0530

    condition and string

---
+++ condition/condit.py
@@ -0,0 +1,34 @@
+
+# age = 18
+
+# if( age <= 18):
+#     print("You are eligibel to voter card")
+# else:
+#     print("you are not eligibel to voter card")
+
+
+# marks = 90
+
+# if(marks >= 90):
+#     print("Grade A")
+# elif(marks >= 80 and marks < 90):
+#     print("Grade B")
+# elif(marks >= 70 and marks < 80):
+#     print("Grade C")
+# elif(marks >= 70 and marks >= 60):
+#     print("Grade D")
+# else:
+#     print("you are not eligibel")
+
+# marks = int(input("enter the marks : "))
+
+# if(marks >= 90):
+#     grade = "A"
+# elif(marks >= 80 and marks < 90):
+#     grade = "B"
+# elif(marks >= 70 and marks < 80):
+#     grade = "C"
+# else:
+#     grade = "D"
+    
+# print("grade the stuent", grade)
---
+++ condition/nestedloop.py
@@ -0,0 +1,38 @@
+# age = int(input("Enter a number : "))
+
+# if(age >= 18):
+#     if(age >= 80):
+#         print("can not drive")
+#     else:
+#         print("can drive")
+# else:
+#     print("cannot drive")
+    
+    
+    #odd or even number
+    
+# num = int(input("enter number : "))
+# if(num % 2 == 0):
+#         print("EVEN")
+# else:
+#         print("ODD")
+
+
+# a = int(input("enter first number : "))
+# b = int(input("enter second number : "))
+# c = int(input("enter third number : "))
+
+# if(a >= b and a >= c):
+#     print("first number is largest",a)
+# elif(b >= c):
+#     print("second number is largest",b)
+# else:
+#     print("third is largest", c)
+
+
+x = int(input("enter a number : "))
+
+if(x % 7 == 0):
+    print("multiple of 7")
+else:
+    print("not a multiple 7")
