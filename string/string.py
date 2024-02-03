2eededb04e58a267b93bf3f7b0ac8ce0608a1d15
Deepak Kumar <127077941+Deepak911741@users.noreply.github.com>
2024-02-03 14:23:45 +0530

    string

---
+++ string/string.py
@@ -0,0 +1,42 @@
+# str1 = "Deepk" #this is a string data type
+# str2 = 'kumar' #this is a string data type
+# str3 = """Deepak""" #this is a string data type
+
+# #concatination
+# len2 = len(str1)
+# print(len2)
+
+# len3 = len(str2)
+# print(len3)
+
+
+# final_str = str1 + " " + str2
+# print(final_str)
+# print(len(final_str))
+
+
+# str = "Deepak kumar"
+# print(str[5:])
+
+# str = "Deepak kumar"
+# print(str[-5:-1])
+
+
+# str = "i am studying python in deepk kumar"
+# print(str.capitalize())
+
+
+# str = "i am studying python in deepk kumar"
+# print(str.replace("a", "k"))
+
+# str = "i am studying python in deepk kumar"
+# print(str.find("z"))
+
+
+# str = "i am studying python in deepk kumar"
+# print(str.count("a"))
+
+
+first_name = input("Enter a line: ")
+print("Lenght of your name is", len(first_name))
+print(first_name)
