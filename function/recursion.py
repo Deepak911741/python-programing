a1484297550e7527978633ca670980b967b9e53d
Deepak Kumar <127077941+Deepak911741@users.noreply.github.com>
2024-02-06 20:04:27 +0530

    recursion

--- function/recursion.py
+++ function/recursion.py
@@ -1,8 +1,34 @@
 #when a function calls itself repeatedly
 
-def show(n):
-    if(n == 0):
+# def show(n):
+#     if(n == 0):
+#         return
+#     print(n)
+#     show(n-1)
+# show(5)
+
+# def fact(n):
+#     if(n == 1 or n == 0):
+#         return 1
+#     return fact(n-1) * n
+# print(fact(4))
+
+
+# def cal_sum(n):
+#     if(n == 0):
+#         return 0
+#     return cal_sum(n-1) + n
+# sum = cal_sum(5)
+
+# print(sum)
+
+
+def print_list(list, idx=0):
+    if(idx == len(list)):
         return
-    print(n)
-    show(n-1)
-show(5)
+    print(list[idx])
+    print_list(list, idx+1)
+    
+fruts = ["mango", "litichi", "apple", "banana"]
+
+print_list(fruts)
